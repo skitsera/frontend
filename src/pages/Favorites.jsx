efeeec62e8881c4f63754bfdea18e0c8a537ebaf
skicera <sergii@acreto.io>
2025-10-06 19:10:21 +0300

    first commit

---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,26 @@
+import "../css/Favorites.css"
+import { useMovieContext } from "../contexts/MovieContext"
+import MovieCard from "../components/MovieCard"
+
+function Favorites() {
+    const {favorites} = useMovieContext()
+
+    if (favorites) {
+        return (
+        <div className="favorites">
+            <h2>Your Favorite Movies</h2>
+            <div className="movies-grid">
+                {favorites.map((movie) =>
+                    <MovieCard movie={movie} key={movie.id} />
+                )}
+            </div>
+        </div>)
+    }
+
+    return <div className="favorites-empty">
+        <h2>No Favorite Movies yet</h2>
+        <p>Start adding moview to your favorites</p>
+    </div>
+}
+
+export default Favorites;
---
+++ src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "47e3b5a4d6108705d112cb098cd74e9e";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
